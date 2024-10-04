efc9811c939016bdc2478be3f77e7d708589dd7c
liuhuo <liuhuo2370@gmail.com>
2024-10-04 17:02:24 +0800

    update: 程序结构调整

--- src/components/home.rs
+++ src/components/home.rs
@@ -1,19 +1,81 @@
-use color_eyre::Result;
-use ratatui::{prelude::*, widgets::*};
-use tokio::sync::mpsc::UnboundedSender;
+use std::{collections::HashMap, time::Duration};
 
 use super::Component;
 use crate::{action::Action, config::Config};
+use color_eyre::Result;
+use crossterm::event::KeyEvent;
+use ratatui::{prelude::*, widgets::*};
+use tokio::sync::mpsc::UnboundedSender;
+use tracing::{debug, info};
+use tui_input::{backend::crossterm::EventHandler, Input};
+
+#[derive(Default, Copy, Clone, PartialEq, Eq)]
+pub enum Mode {
+    #[default]
+    Normal,
+    Insert,
+    processing,
+}
 
 #[derive(Default)]
 pub struct Home {
     command_tx: Option<UnboundedSender<Action>>,
     config: Config,
+    pub show_help: bool,
+    pub counter: usize,
+    pub app_ticker: usize,
+    pub render_ticker: usize,
+    pub mode: Mode,
+    pub input: Input,
+    pub action_tx: Option<UnboundedSender<Action>>,
+    pub keymap: HashMap<KeyEvent, Action>,
+    pub text: Vec<String>,
+    pub last_events: Vec<KeyEvent>,
 }
 
 impl Home {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    pub fn keymap(mut self, keymap: HashMap<KeyEvent, Action>) -> Self {
+        self.keymap = keymap;
+        self
+    }
+
+    pub fn tick(&mut self) {
+        info!("Tick");
+        self.app_ticker = self.app_ticker.saturating_add(1);
+        self.last_events.drain(..);
+    }
+
+    pub fn render_tick(&mut self) {
+        debug!("Render Tick");
+        self.render_ticker = self.render_ticker.saturating_add(1);
+    }
+
+    pub fn add(&mut self, s: String) {
+        self.text.push(s);
+    }
+
+    pub fn schedule_increment(&mut self, i: usize) {
+        let tx = self.action_tx.clone().unwrap();
+        tokio::spawn(async move {
+            tx.send(Action::EnterProcessing).unwrap();
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            tx.send(Action::Increment(i)).unwrap();
+            tx.send(Action::ExitProcessing).unwrap();
+        });
+    }
+
+    pub fn schedule_decrement(&mut self, i: usize) {
+        let tx = self.action_tx.clone().unwrap();
+        tokio::spawn(async move {
+            tx.send(Action::EnterProcessing).unwrap();
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            tx.send(Action::Decrement(i)).unwrap();
+            tx.send(Action::ExitProcessing).unwrap();
+        });
     }
 }
 
